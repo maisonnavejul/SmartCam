6e8c39bea10cab14756550270c44664f5d970f7d
Jules MAISONNAVE <jules.maisonnave@student.junia.com>
2024-01-30 16:30:38 +0100

    s

---
+++ smartcamfront/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className='test'>SmartCam</h1>
+        <p>Hello world!</p>
+      </div>
+    );
+  }
+}
+export default App;
