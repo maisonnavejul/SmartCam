3b8b24da85b270485f8f53bbf11b2f100c7af2cf
Jules MAISONNAVE <jules.maisonnave@student.junia.com>
2024-02-14 12:45:29 +0100

    up

--- app.py
+++ app.py
@@ -13,15 +13,58 @@ mqtt_broker = "test.mosquitto.org"
 mqtt_port = 1883
 mqtt_client = mqtt.Client()
 
+nbpers = None
+temperature = None
+humidity = None
+light = "OFF"
+chauffage = "OFF"
+chauffage_from_front = "NO"
+
 bouton_active = False
 
 def on_connect(client, userdata, flags, rc):
     print("Connexion au broker MQTT établie avec le code de retour : " + str(rc))
-    client.subscribe("test/topic")
-    print("Souscription au topic 'iot/data'")
+    client.subscribe("smartcam/data")
+    print("Souscription au topic 'smartcam/data'")
     
 def on_message(client, userdata, msg):
-    print(f"Message reçu : {msg.topic} {msg.payload.decode()}")
+    global nbpers, temperature, humidity 
+    try:
+        message_data = json.loads(msg.payload.decode())
+        
+        if message_data['type'] == 'message':
+            print(f"Message général reçu : {message_data['data']}")
+            
+        elif message_data['type'] == 'temperature':
+            temperature = float(message_data['data'])
+            print(f"Température mise à jour : {temperature}")
+            
+        elif message_data['type'] == 'humidity':
+            humidity = float(message_data['data'])
+            print(f"Humidité mise à jour : {humidity}")
+            
+        elif message_data['type'] == 'nbpers':
+            nbpers = int(message_data['data'])
+            print(f"Nombre de personnes mis à jour : {nbpers}")
+            
+        if None not in (nbpers, temperature, humidity):
+            algo(nbpers, temperature, humidity)
+            nbpers = None
+            temperature = None
+            humidity = None
+    
+
+            
+    except json.JSONDecodeError:
+        print("Le message reçu n'est pas un JSON valide.")
+    except ValueError as e:
+        print("Erreur lors de la conversion du message : ", e)
+    except KeyError as e:
+        print("Clé manquante dans le message JSON : ", e)
+    except TypeError as e:
+        print("Erreur de type de données : ", e)
+
+
     
 mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
@@ -32,14 +75,37 @@ mqtt_client.loop_start()
 
 CORS(app)
 
-nbper = 0
-temperature = 20
-humidity = 12
-light = "OFF"
-chauffage = "OFF"
-chauffage_from_front = "NO"
-
-
+def algo(nbpers, temperature, humidity):
+    
+    chauffage_from_front = last_data_chauffage_from_front()
+    
+    print("Chauffage from front: ", chauffage_from_front)
+    if chauffage_from_front == "NO":    
+        if temperature <= 17:
+            chauffage = "ON"
+        else:
+            chauffage = "OFF"
+        if nbpers > 0 :
+            light = "ON"
+        else:
+            light = "OFF"
+            
+        #insert_data(nbpers, temperature, humidity, light, chauffage, chauffage_from_front)
+        print("Nbper: ", nbpers , "Temperature: ", temperature, "Humidity: ", humidity, "Light: ", light, "Chauffage: ", chauffage, "Chauffage from front: ", chauffage_from_front)
+    if chauffage_from_front == "YES":
+        if temperature >= 24:
+            chauffage = "OFF"
+        else:
+            chauffage = "ON"
+        if nbpers > 0 :
+            light = "ON"
+        else:
+            light = "OFF"
+        #insert_data(nbpers, temperature, humidity, light, chauffage, chauffage_from_front)
+        print("Nbper: ", nbpers , "Temperature: ", temperature, "Humidity: ", humidity, "Light: ", light, "Chauffage: ", chauffage, "Chauffage from front: ", chauffage_from_front)
+            
+    
+    
 @app.route('/', methods=['GET'])
 def index():
     return 'SmartCam Server is running :)'
@@ -54,21 +120,25 @@ def post_temperature():
 def test():
     return jsonify({"chauffage_from_front": chauffage_from_front})
 
-#recupération chauffage from front
+
 @app.route('/sendchauffage', methods=['POST'])
 def post_chauffage():
-    global chauffage_from_front
+
     data = request.get_json()
     print(data)
     chauffage_from_front = data['chauffage']
-    #insert_data(nbper, temperature, humidity, light, chauffage, chauffage_from_front)
+    print("Chauffage from front send: ", chauffage_from_front)
+
+    
+    insert_data_chauffage(chauffage_from_front)
+
     return jsonify(data)
 
 @app.route('/insertdataTEST', methods=['GET'])
 def insertdata():
     print("Inserting data...")
-    print("Nbper: ", nbper , "Temperature: ", temperature, "Humidity: ", humidity, "Light: ", light, "Chauffage: ", chauffage, "Chauffage from front: ", chauffage_from_front)
-    insert_data(nbper, temperature, humidity, light, chauffage, chauffage_from_front)
+    print("Nbper: ", nbpers , "Temperature: ", temperature, "Humidity: ", humidity, "Light: ", light, "Chauffage: ", chauffage, "Chauffage from front: ", chauffage_from_front)
+    insert_data(nbpers, temperature, humidity, light, chauffage, chauffage_from_front)
     return 'Data inserted successfully.'
 
 @app.route('/getdata', methods=['GET'])
@@ -78,6 +148,5 @@ def allData():
 
 
 if __name__ == '__main__':
-    app.run(host='10.224.0.99', port=5000)
-while True:
-    pass
+    app.run(host='10.224.0.99', port=5000, debug=True)  # Activez le mode debug pour le développement
+
--- database.py
+++ database.py
@@ -7,6 +7,7 @@ def create_database():
         conn = sqlite3.connect('database.db')
         cursor = conn.cursor()
         cursor.execute("CREATE TABLE IF NOT EXISTS info_salle_1 ( id INTEGER PRIMARY KEY, nbpers TEXT DEFAULT '0', temperature TEXT DEFAULT '0', humidity TEXT DEFAULT '0', light TEXT DEFAULT 'OFF',chauffage TEXT DEFAULT 'OFF',chauffage_from_front TEXT DEFAULT 'NO', date TEXT)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS chauffage ( id INTEGER PRIMARY KEY, chauffage_from_front TEXT DEFAULT 'OFF', date TEXT)")
         conn.close()
         print("Database created successfully.")
     except Exception as e:
@@ -39,6 +40,38 @@ def all_data():
     except Exception as e:
         print("Error fetching data:", str(e))
         return None
+def insert_data_chauffage(chauffage_from_front):
+    try:
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        
+        # Insertion d'une ligne de données
+        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        cursor.execute("INSERT INTO chauffage (chauffage_from_front, date) VALUES (?, ?)",
+                       (chauffage_from_front, now))
+        
+        conn.commit()  
+        conn.close()
+        print("Data inserted successfully.")
+    except Exception as e:
+        print("Error inserting data:", str(e))
+        
+def last_data_chauffage_from_front():
+    try:
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        # Fetch the last row's 'chauffage_from_front' column value
+        cursor.execute("SELECT chauffage_from_front FROM chauffage ORDER BY id DESC LIMIT 1")
+        row = cursor.fetchone()  # fetchone() retrieves a single row
+        conn.close()
+        if row:  # Check if a row was fetched
+            return row[0]  # Return the 'chauffage_from_front' value ('yes' or 'no')
+        else:
+            return "No data"  # Return a message if no data was found
+    except Exception as e:
+        print("Error fetching data:", str(e))
+        return None
+
 
 if __name__ == '__main__':
     create_database()
