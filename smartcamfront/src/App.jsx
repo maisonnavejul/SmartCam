8b02d64a9e5b4351ce788f435f207867c1e423ef
Jules MAISONNAVE <jules.maisonnave@student.junia.com>
2024-03-15 17:49:21 +0100

    save

--- smartcamfront/src/App.jsx
+++ smartcamfront/src/App.jsx
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
 import './App.css';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 class App extends Component {
   constructor(props) {
     super(props);
+    const savedTemperatureData = JSON.parse(localStorage.getItem('temperatureData')) || [];
     this.state = {
       textValue: '',
       errorMessage: '',
       allData: [], // Initialisation d'un état pour stocker toutes les données
       chauffageState: 'NO', // Ajout d'un nouvel état pour le chauffage
       chauffageState2: 'YES',
+      currentTemperature: 'N/A', // Ajout pour la température actuelle
+      isPersonPresent: false, // Ajout pour la présence de personne
+      temperatureData: savedTemperatureData,
     };
     
   }
+
   toggleChauffage = async () => {
     const newState = this.state.chauffageState === 'NO' ? 'YES' :'YES';
     this.setState({ chauffageState: newState });
     await this.postChauffageState(newState);
   };
+
   postChauffageState = async (state) => {
     try {
-      const response = await fetch('http://192.168.1.39:5000/sendchauffage', {
+      const response = await fetch('http://172.20.10.5:5000/sendchauffage', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -43,7 +70,7 @@ class App extends Component {
   }
   postChauffageState2 = async (state) => {
     try {
-      const response = await fetch('http://192.168.1.39:5000/sendchauffage', {
+      const response = await fetch('http://172.20.10.5:5000/sendchauffage', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -61,12 +88,52 @@ class App extends Component {
     }
   };
 
+  getTemperatureData = async () => {
+    try {
+      const response = await fetch('http://172.20.10.5:5000/getiftemp');
+      const data = await response.json();
+  
+      // Mettre à jour directement currentTemperature avec la valeur obtenue
+      this.setState({
+        currentTemperature: parseFloat(data.temperature).toFixed(1) // Arrondi à une décimale
+      });
+  
+      // Ajoutez également cette nouvelle donnée à temperatureData pour le graphique, si nécessaire
+      const newTemperatureData = {
+        date: new Date().toLocaleString(), // Date et heure actuelles pour l'exemple
+        temperature: data.temperature,
+      };
+      this.setState(prevState => ({
+        temperatureData: [...prevState.temperatureData, newTemperatureData],
+      }));
+  
+      // Sauvegarde dans localStorage pour persistance
+      localStorage.setItem('temperatureData', JSON.stringify(this.state.temperatureData));
+  
+    } catch (error) {
+      console.error('Erreur lors de la récupération des données de température', error);
+      this.setState({ errorMessage: 'Erreur lors de la récupération des données de température.' });
+    }
+  };
+  
+  
   
 
   
   componentDidMount() {
     this.getAllData(); // Appeler getAllData lorsque le composant est monté
+    this.getPresenceData(); // Appeler getPresenceData également lors du montage
+    this.dataInterval = setInterval(this.getAllData, 10000); // Récupérer toutes les données toutes les 10 secondes
+    this.presenceInterval = setInterval(this.getPresenceData, 2000); // Vérifier la présence toutes les 5 secondes
+    this.tempInterval = setInterval(this.getTemperatureData, 3000); // Par exemple, toutes les 10 secondes
   }
+  
+  componentWillUnmount() {
+    clearInterval(this.dataInterval); // Nettoyer l'intervalle pour getAllData
+    clearInterval(this.presenceInterval); // Nettoyer l'intervalle pour getPresenceData
+    clearInterval(this.tempInterval);
+  }
+  
 
   handleInputChange = (event) => {
     const value = event.target.value;
@@ -84,11 +151,12 @@ class App extends Component {
       this.setState({ textValue: '', errorMessage: '' });
     }
   }
+  
 
   postTemperature = async (temperature) => {
     console.log('temperature en envoi:', temperature);
     try {
-      const response = await fetch('http://192.168.1.39:5000/sendtemperature', {
+      const response = await fetch('http://172.20.10.5:5000/sendtemperature', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -109,19 +177,95 @@ class App extends Component {
   getAllData = async () => {
     console.log('Récupération de toutes les données');
     try {
-      const response = await fetch('http://192.168.1.39:5000/getdata', {
+      const response = await fetch('http://172.20.10.5:5000/getdata', {
         method: 'GET',
       });
       const data = await response.json();
       console.log('Réponse du serveur:', data);
-      this.setState({ allData: data }); 
+      this.setState({ allData: data });
+      if (data.length > 0) {
+        const latestData = data[data.length - 1]; // Supposons que le dernier élément contient les dernières infos
+        this.setState({
+          currentTemperature: parseFloat(latestData[2]).toFixed(1), // Arrondi à une décimale
+          isPersonPresent: latestData[1] > 0,
+        });
+      }
     } catch (error) {
       console.error('Erreur lors de la récupération des données', error);
       this.setState({ errorMessage: 'Erreur lors de la récupération des données.' });
     }
   }
-
+  
+  getPresenceData = async () => {
+    try {
+      const response = await fetch('http://172.20.10.5:5000/getifdetect', {
+        method: 'GET',
+      });
+      const data = await response.json();
+      console.log('Réponse de présence du serveur:', data);
+  
+      // Mettre à jour l'état de la présence en fonction de la réponse
+      this.setState({
+        isPersonPresent: data.detection > 0, // Supposons que 'detection' retourne 1 pour présence, 0 sinon
+      });
+    } catch (error) {
+      console.error('Erreur lors de la récupération de la présence', error);
+      this.setState({ errorMessage: 'Erreur lors de la récupération des données de présence.' });
+    }
+  }
+  resetGraph = () => {
+    this.setState({ temperatureData: [] }); // Réinitialiser les données de température
+    localStorage.removeItem('temperatureData'); // Optionnel: Supprimer de localStorage pour une réinitialisation persistante
+  };
+  
   render() {
+    const data = {
+      labels: this.state.temperatureData.map(item => item.date),
+      datasets: [{
+        label: 'Temperature',
+        data: this.state.temperatureData.map(item => item.temperature),
+        fill: false,
+        backgroundColor: 'rgb(255, 0, 0,0.9)',
+        borderColor: 'rgba(255, 0, 0, 0.4)',
+      }],
+    };
+  
+  
+    const options = {
+      scales: {
+        y: {
+          beginAtZero: true,
+          grid: {
+            color: 'white', // Définir la couleur de la grille de l'axe Y en blanc
+          },
+          ticks: {
+            color: 'white', // Définir la couleur des labels de l'axe Y en blanc
+          }
+        },
+        x: {
+          grid: {
+            color: 'white', // Définir la couleur de la grille de l'axe X en blanc
+          },
+          ticks: {
+            color: 'rgba(128, 128, 128, 1)', // Définir la couleur des labels de l'axe X en blanc
+          }
+        }
+      },
+      plugins: {
+        legend: {
+          labels: {
+            color: 'white' // Définir la couleur des labels de la légende en blanc
+          }
+        },
+        tooltip: {
+          titleColor: 'white', // Couleur du titre du tooltip
+          bodyColor: 'white', // Couleur du corps du tooltip
+          footerColor: 'white', // Couleur du pied de page du tooltip
+        }
+      }
+    };
+    
+
     return (
       <div className='Header'>
         <div className='Title'>
@@ -136,11 +280,37 @@ class App extends Component {
             value={this.state.textValue}
             onChange={this.handleInputChange}
           />
-          <button onClick={this.handleSubmit}>Envoyer</button>
+          <div className='buttonSend'>
+          <button onClick={this.handleSubmit}>Send</button>
+          </div>
           {this.state.errorMessage && <p style={{color: 'red'}}>{this.state.errorMessage}</p>}
         </div>
-        <button onClick={this.toggleChauffage}>Start Chauffage</button>
-        <button onClick={this.toggleChauffage2}>Stop Chauffage</button>
+        <div className="buttonsContainer">
+        <button onClick={this.toggleChauffage}>Start Heating</button>
+        <button onClick={this.toggleChauffage2}>Stop Heating</button>
+      </div>
+
+
+        <div className='infoBlocks'>
+        <div className='temperatureBlock'>
+        <h3>Current Temperature</h3>
+        <p>{this.state.currentTemperature}°C</p>
+      </div>
+
+          <div className='presenceBlock'>
+            <h3>Occuped Room</h3>
+            <p>{this.state.isPersonPresent ? 'Yes' : 'No'}</p>
+          </div>
+        </div>
+
+        <div className='chartContainer'>
+          <h2>Evolution of temperature</h2>
+          <Line data={data} options={options} />
+        </div>
+        <div className='resetButtonContainer'>
+        <button onClick={this.resetGraph}>Reset data graph</button>
+        </div>
+
 
 
         <div className='AllData'>
@@ -149,7 +319,7 @@ class App extends Component {
           
           <div className='rectangle'>
           <div key={index}>
-            <p>ID: {item[0]} - people: {item[1]} - Temperature: {item[2]} -Humidity: {item[3]} Light: {item[4]} -Chauffage: {item[5]} -Date: {item[6]}</p>
+            <p>ID: {item[0]} - Occuped: {item[1]} - Temperature: {item[2]} -Humidity: {item[3]} Light: {item[4]} -Heating: {item[5]} -Date: {item[6]}</p>
           </div>
           </div>
         ))}
